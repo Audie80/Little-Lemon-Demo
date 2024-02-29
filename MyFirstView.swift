b386d94e21e33a8ecd436223e13b2cc9f3a6acf3
Aude Velly Menut <49475179+Audie80@users.noreply.github.com>
2024-02-29 18:39:30 +0100

    Create MyFirstView.swift

---
+++ MyFirstView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct MyFirstView: View {
+  var body: some View {
+    VStack {
+      Text("Little Lemon")
+    }
+  }
+}
+
+struct MyFirstView_Previews: PreviewProvider {
+  static var previews: some View {
+    MyFirstView()
+  }
+}
